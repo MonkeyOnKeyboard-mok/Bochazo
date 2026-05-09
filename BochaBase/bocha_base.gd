ae069136a838305b3ebd3a08d5ac44b3aa4bd4fc
MonkeyOnKeyboard-mok <santi.ortega.serra@gmail.com>
2026-05-09 18:28:21 -0300

    2sd

--- BochaBase/bocha_base.gd
+++ BochaBase/bocha_base.gd
@@ -1,6 +1,8 @@
 extends Node3D
 class_name Bocha
 
+@export var speed := 5.0
+@onready var bocha_rb: RigidBody3D = $BochaRigidBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,5 +10,14 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+func _physics_process(_delta: float) -> void:
+	var direction := 0.0
+	if Input.is_action_pressed("move_left"):
+		print("pressing left")
+		direction -= 1.0
+	if Input.is_action_pressed("move_right"):
+		direction += 1.0
+	bocha_rb.linear_velocity.x = direction * speed
