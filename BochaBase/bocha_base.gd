c6b908db799708f3bd26fc444f4d45990ed17fee
MonkeyOnKeyboard-mok <santi.ortega.serra@gmail.com>
2026-05-09 19:03:51 -0300

    asd

--- BochaBase/bocha_base.gd
+++ BochaBase/bocha_base.gd
@@ -1,23 +1,10 @@
 extends Node3D
 class_name Bocha
 
-@export var speed := 5.0
-@onready var bocha_rb: RigidBody3D = $BochaRigidBody
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
-func _physics_process(_delta: float) -> void:
-	var direction := 0.0
-	if Input.is_action_pressed("move_left"):
-		print("pressing left")
-		direction -= 1.0
-	if Input.is_action_pressed("move_right"):
-		direction += 1.0
-	bocha_rb.linear_velocity.x = direction * speed
---
+++ BochaBase/input_manager.gd
@@ -0,0 +1,36 @@
+extends Node3D
+class_name BochaInputManager
+
+@export var speed : float = 5.0
+@export var throw_speed : float = 10.0
+@export var bocha_rb: RigidBody3D
+
+var throwed : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _physics_process(_delta: float) -> void:
+	if !throwed:
+		_move_before_throw()
+		_throw()
+
+func _move_before_throw() ->void:
+	var direction := 0.0
+	if Input.is_action_pressed("move_left"):
+		direction += 1.0
+	if Input.is_action_pressed("move_right"):
+		direction -= 1.0
+	bocha_rb.linear_velocity.z = direction * speed
+
+func _throw() -> void:
+	var direction := 0.0
+	if Input.is_action_pressed("start_throw"):
+		throwed = true
+		direction -= 1.0
+		bocha_rb.linear_velocity.x = direction * throw_speed
