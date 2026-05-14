1f8f087fb5a0494564ff2ff6fc389d685c83a210
GianfrancoFrattini <gianfranco.frattini97@gmail.com>
2026-05-13 22:46:43 -0300

    Fases 1.6 y 1.8 completadas y 2.1
    main_menu.tscn tiene los placeholders de botones
    Play
    Options
    Exit
    
    fade_transition.tscn ya esta creada
    tiene dos funciones fade_in() y fade_out()
    
    Para agregar dentro de las escenas y realizar las transiciones
    
    Sin embargo, existe un bug el cual es el parpadeo de los fade.
    
    Tambien cree court.tscn
    
    De manera sencilla.

--- scenes/00_boot/boot.gd
+++ scenes/00_boot/boot.gd
@@ -1,24 +1,9 @@
 extends Node
 
 @export_file("*.tscn") var next_scene: String = "res://scenes/01_main_menu/main_menu.tscn"
-@export var transition_duration: float = 3
-@export var debug_verbose: bool = false
+@onready var fade: FadeTransition = $FadeTransition
 
-@onready var fade_rect: ColorRect = $CanvasLayer/FadeRect
-@onready var anim_player: AnimationPlayer = $CanvasLayer/AnimPlayer
-
-# Eliminá el AnimationPlayer de la escena y usá esto en boot.gd:
 func _ready():
-	if debug_verbose: print("[Boot] Iniciando carga...")
-	await get_tree().process_frame
-	
-	# Fade a negro usando Tween (Godot 4 standard)
-	var tween = create_tween()
-	tween.tween_property(fade_rect, "color", Color(0,0,0,1), transition_duration)
-	await tween.finished
-	
-	if next_scene.is_empty() or not FileAccess.file_exists(next_scene):
-		push_error("[Boot] Escena destino no existe: %s" % next_scene)
-		return
-		
+	await get_tree().process_frame # Dar tiempo a Autoloads
+	await fade.fade_out()
 	get_tree().change_scene_to_file(next_scene)
