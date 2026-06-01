51b42398c3263b5feb8f9de3f186a397e7bd0c70
MonkeyOnKeyboard-mok <santi.ortega.serra@gmail.com>
2026-06-01 10:59:54 -0300

    turnos UI WIP

---
+++ scenes/04_game/components/UI/turnos.gd
@@ -0,0 +1,16 @@
+extends Control
+
+const AZUL = preload("uid://cia10v6ul850a")
+const ROJA = preload("uid://wpqu8kkku4tv")
+
+@onready var h_box1: HBoxContainer = $HBoxPlayer1
+@onready var h_box2: HBoxContainer = $HBoxPlayer2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
